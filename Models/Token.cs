643a16abddbf2863f3efb620b966bbd958b155b8
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2023-10-17 14:04:42 +0200

    If two tokens stand on the same tile they block the path for the other teams.

--- Models/Token.cs
+++ Models/Token.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Text;
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI;
@@ -42,25 +44,26 @@ namespace FiaMedKnuffGrupp4.Models
 
         public void MoveToken(Token token, int diceRollResult, Grid grid, List<Token> allTokens)
         {
+            bool canContinueMoving = true;
             int tile = grid.GetTile(CurrentPositionRow, CurrentPositionCol);
-            if(diceRollResult == 6 && tile == 0) 
-            { 
-                if(token.TokenColor == Colors.Red)
+            if (diceRollResult == 6 && tile == 0)
+            {
+                if (token.TokenColor == Colors.Red)
                 {
                     CurrentPositionCol = 6;
                     CurrentPositionRow = 13;
                 }
-                else if(token.TokenColor == Colors.Green)
+                else if (token.TokenColor == Colors.Green)
                 {
                     CurrentPositionCol = 1;
                     CurrentPositionRow = 6;
                 }
-                else if(token.TokenColor == Colors.Blue)
+                else if (token.TokenColor == Colors.Blue)
                 {
                     CurrentPositionCol = 13;
                     CurrentPositionRow = 8;
                 }
-                else if(token.TokenColor == Colors.Yellow)
+                else if (token.TokenColor == Colors.Yellow)
                 {
                     CurrentPositionCol = 8;
                     CurrentPositionRow = 1;
@@ -69,41 +72,64 @@ namespace FiaMedKnuffGrupp4.Models
             }
             //1 = Up, 2 = Right,3 = up & right 4 = Down, 5 = down / right 6 = left / up 8 = Left 9 = left / down 11
             //Only move as many cells as the dice roll result.
-            for(int i = 0; i < diceRollResult; i++)
+            int tempRow = CurrentPositionRow;
+            int tempCol = CurrentPositionCol;
+
+            for (int i = 0; i < diceRollResult; i++)
             {
-                switch(tile)
+                switch (tile)
                 {
                     case 1:
-                        CurrentPositionRow--;
+                        tempRow--;
                         break;
                     case 2:
-                        CurrentPositionCol++;
+                        tempCol++;
                         break;
                     case 3:
-                        CurrentPositionCol++;
-                        CurrentPositionRow--;
+                        tempCol++;
+                        tempRow--;
                         break;
                     case 4:
-                        CurrentPositionRow++;
+                        tempRow++;
                         break;
                     case 5:
-                        CurrentPositionCol++;
-                        CurrentPositionRow++;
+                        tempCol++;
+                        tempRow++;
                         break;
                     case 6:
-                        CurrentPositionCol--;
-                        CurrentPositionRow--;
+                        tempCol--;
+                        tempRow--;
                         break;
                     case 8:
-                        CurrentPositionCol--;
+                        tempCol--;
                         break;
                     case 9:
-                        CurrentPositionCol--;
-                        CurrentPositionRow++;
+                        tempCol--;
+                        tempRow++;
                         break;
                 }
-                tile = grid.GetTile(CurrentPositionRow, CurrentPositionCol);
+
+                tile = grid.GetTile(tempRow, tempCol);
+
+                var tokensAtNextPosition = allTokens.Where(t =>
+                   t.CurrentPositionRow == tempRow &&
+                   t.CurrentPositionCol == tempCol).ToList();
+
+                // Check if two tokens of a different color block the path
+                if (tokensAtNextPosition.Count >= 2 && tokensAtNextPosition.Any(t => t.TokenColor != token.TokenColor))
+                {
+                    canContinueMoving = false;
+                    break;
+                }
+                // If can continue, update the token's current position.
+                if (canContinueMoving)
+                {
+                    CurrentPositionRow = tempRow;
+                    CurrentPositionCol = tempCol;
+                }
+            
             }
+
 
             foreach (var otherToken in allTokens)
             {
@@ -179,7 +205,7 @@ namespace FiaMedKnuffGrupp4.Models
             t.CurrentPositionCol == this.CurrentPositionCol &&
             t.TokenColor == this.TokenColor).ToList();
 
-            if(tokensAtSamePosition.Count > 1)
+            if (tokensAtSamePosition.Count > 1)
             {
                 if (tokensAtSamePosition[0] == this)
                 {
@@ -190,7 +216,7 @@ namespace FiaMedKnuffGrupp4.Models
                 {
                     tokenX -= cellSize * 0.1f;
                     tokenY -= cellSize * 0.1f;
-                }   
+                }
             }
             // Define the token's radius and color.
             Color tokenColor = TokenColor;
